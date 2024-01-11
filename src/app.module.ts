bff1f4c5c1603382a8868b38007cc9a01a8c6858
Opeyemi0002 <adekoyejoseun@gmail.com>
2024-01-11 03:17:58 +0100

    setup/database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ApplicantModule } from './applicant/applicant.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          type: 'sqlite',
+          database: config.get<string>('DB_NAME'),
+          synchronize: true,
+          entities: [],
+        };
+      },
+    }),
+    ApplicantModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
